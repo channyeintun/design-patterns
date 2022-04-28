5c263b669b9aa16a9309512851a717116f654844
Chan Nyein Tun <chanyeintun@gmail.com>
2022-04-28 20:05:11 +0630

    Memento Pattern

---
+++ src/main/java/memento/MementoDemo.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package memento;
+
+/**
+ *
+ * @author channyeintun
+ */
+public class MementoDemo {
+
+    public static void main(String[] args) {
+        TextEditor textEditor = new TextEditor(new TextWindow());
+        textEditor.write("The Memento Design Pattern\n");
+        textEditor.write("How to implement it in Java?\n");
+        textEditor.hitSave();
+
+        textEditor.write("Buy milk and eggs before coming home\n");
+
+        textEditor.hitUndo();
+        System.out.println(textEditor.print());
+    }
+}
