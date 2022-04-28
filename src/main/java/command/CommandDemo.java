bae68f87f91ed994caa7711a92d8cfee9544f920
Chan Nyein Tun <chanyeintun@gmail.com>
2022-04-28 12:31:55 +0630

    Command Pattern

---
+++ src/main/java/command/CommandDemo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package command;
+
+/**
+ *
+ * @author channyeintun
+ */
+public class CommandDemo {
+     public static void main(String[] args) {
+        Editor editor = new Editor();
+        editor.init();
+    }
+}
