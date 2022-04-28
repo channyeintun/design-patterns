77d8f3541564c5e8608062bd4d77ff2ddf7bd1a8
Chan Nyein Tun <chanyeintun@gmail.com>
2022-04-28 10:13:16 +0630

    Flyweight Pattern

---
+++ src/main/java/flyweight/TreeFactory.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package flyweight;
+
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author channyeintun
+ */
+
+public class TreeFactory {
+
+    static Map<String, TreeType> treeTypes = new HashMap<>();
+
+    public static TreeType getTreeType(String name, Color color, String otherTreeData) {
+        TreeType result = treeTypes.get(name);
+        if (result == null) {
+            result = new TreeType(name, color, otherTreeData);
+            treeTypes.put(name, result);
+        }
+        return result;
+    }
+}
